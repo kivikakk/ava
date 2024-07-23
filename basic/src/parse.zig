115cc8af449dc7172ea6118adb6aa83c4fe3e19a
Asherah Connor <ashe@kivikakk.ee>
2024-07-23 11:49:46 +0300

    parse unary.

--- basic/src/parse.zig
+++ basic/src/parse.zig
@@ -9,13 +9,21 @@ pub const Imm = union(enum) {
     number: isize,
 };
 
-pub const Node = struct {
-    value: union(enum) {
-        call: struct {
-            name: WithRange([]const u8),
-            args: []const Imm,
-        },
+pub const Node = union(enum) {
+    const Self = @This();
+
+    call: struct {
+        name: WithRange([]const u8),
+        args: []const WithRange(Imm),
     },
+
+    pub fn deinit(self: Self, allocator: Allocator) void {
+        switch (self) {
+            .call => |c| {
+                allocator.free(c.args);
+            },
+        }
+    }
 };
 
 pub const Error = error{
@@ -24,10 +32,23 @@ pub const Error = error{
 
 const State = union(enum) {
     init,
-    label: WithRange([]const u8),
+    call: struct {
+        label: WithRange([]const u8),
+        args: std.ArrayList(WithRange(Imm)),
+        comma_next: bool,
+    },
+
+    fn deinit(self: State) void {
+        switch (self) {
+            .init => {},
+            .call => |c| {
+                c.args.deinit();
+            },
+        }
+    }
 };
 
-pub fn parse(allocator: std.mem.Allocator, s: []const u8) ![]Node {
+pub fn parse(allocator: Allocator, s: []const u8) ![]Node {
     var nx = std.ArrayList(Node).init(allocator);
     errdefer nx.deinit();
 
@@ -35,26 +56,35 @@ pub fn parse(allocator: std.mem.Allocator, s: []const u8) ![]Node {
     defer allocator.free(tx);
 
     var state: State = .init;
+    defer state.deinit();
+
     var i: usize = 0;
     while (i < tx.len) : (i += 1) {
         const t = tx[i];
         switch (state) {
             .init => {
                 switch (t.payload) {
-                    .label => |l| state = .{ .label = WithRange([]const u8).init(l, t.range) },
+                    .label => |l| state = .{ .call = .{
+                        .label = WithRange([]const u8).init(l, t.range),
+                        .args = std.ArrayList(WithRange(Imm)).init(allocator),
+                        .comma_next = false,
+                    } },
+                    .linefeed => {},
                     else => return Error.UnexpectedToken,
                 }
             },
-            .label => |l| {
+            .call => |*c| {
                 switch (t.payload) {
-                    .linefeed, .semicolon => {
-                        try nx.append(.{
-                            .value = .{ .call = .{
-                                .name = l,
-                                .args = &.{},
-                            } },
-                        });
-                    },
+                    .linefeed, .semicolon => try nx.append(.{
+                        .call = .{
+                            .name = c.label,
+                            .args = try c.args.toOwnedSlice(),
+                        },
+                    }),
+                    .number => |n| try c.args.append(.{
+                        .payload = .{ .number = n },
+                        .range = t.range,
+                    }),
                     else => return Error.UnexpectedToken,
                 }
             },
@@ -70,7 +100,7 @@ test "parses a nullary statement without line-number" {
 
     try testing.expectEqualDeep(nx, &[_]Node{
         .{
-            .value = .{ .call = .{
+            .call = .{
                 .name = .{
                     .payload = "PRINT",
                     .range = .{
@@ -79,7 +109,38 @@ test "parses a nullary statement without line-number" {
                     },
                 },
                 .args = &.{},
-            } },
+            },
+        },
+    });
+}
+
+test "parses a unary statement without line-number" {
+    const nx = try parse(testing.allocator, "\n PRINT 42\n");
+    defer {
+        for (nx) |n| n.deinit(testing.allocator);
+        testing.allocator.free(nx);
+    }
+
+    try testing.expectEqualDeep(nx, &[_]Node{
+        .{
+            .call = .{
+                .name = .{
+                    .payload = "PRINT",
+                    .range = .{
+                        .start = .{ .row = 2, .col = 2 },
+                        .end = .{ .row = 2, .col = 6 },
+                    },
+                },
+                .args = &.{
+                    .{
+                        .payload = .{ .number = 42 },
+                        .range = .{
+                            .start = .{ .row = 2, .col = 8 },
+                            .end = .{ .row = 2, .col = 9 },
+                        },
+                    },
+                },
+            },
         },
     });
 }
