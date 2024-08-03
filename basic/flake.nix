c615f7b61adc0c45d45ad9d89d4bf17fcc86fc3e
Asherah Connor <ashe@kivikakk.ee>
2024-08-03 15:25:37 +0300

    flake.nix: add.

---
+++ basic/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Ava BASIC development shell";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        name = "ava";
+        nativeBuildInputs = [pkgs.zig];
+      };
+    });
+}
