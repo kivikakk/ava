b147e76d1ac35bfa11cce007c2fd300937eb41c6
Asherah Connor <ashe@kivikakk.ee>
2024-08-04 13:56:39 +0300

    flake.nix: add build.

--- basic/flake.nix
+++ basic/flake.nix
@@ -9,10 +9,14 @@
     zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
     zig-overlay.inputs.flake-utils.follows = "flake-utils";
 
-    zls.url = github:zigtools/zls;
-    zls.inputs.nixpkgs.follows = "nixpkgs";
-    zls.inputs.flake-utils.follows = "flake-utils";
-    zls.inputs.zig-overlay.follows = "zig-overlay";
+    zls-flake.url = github:zigtools/zls;
+    zls-flake.inputs.nixpkgs.follows = "nixpkgs";
+    zls-flake.inputs.flake-utils.follows = "flake-utils";
+    zls-flake.inputs.zig-overlay.follows = "zig-overlay";
+    zls-flake.inputs.gitignore.follows = "gitignore";
+
+    gitignore.url = github:hercules-ci/gitignore.nix;
+    gitignore.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
@@ -20,20 +24,42 @@
     nixpkgs,
     flake-utils,
     zig-overlay,
-    zls,
+    zls-flake,
+    gitignore,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
       zig = zig-overlay.packages.${system}.master;
-    in {
+      zls = zls-flake.packages.${system}.zls;
+      gitignoreSource = gitignore.lib.gitignoreSource;
+    in rec {
       formatter = pkgs.alejandra;
 
       devShells.default = pkgs.mkShell {
         name = "ava";
         nativeBuildInputs = [
-          zig-overlay.packages.${system}.master
-          zls.packages.${system}.zls
+          zig
+          zls
         ];
+      };
+
+      packages.default = packages.ava;
+      packages.ava = pkgs.stdenvNoCC.mkDerivation {
+        name = "ava";
+        version = "main";
+        src = gitignoreSource ./.;
+        nativeBuildInputs = [zig];
+        dontConfigure = true;
+        dontInstall = true;
+        doCheck = true;
+        buildPhase = ''
+          mkdir -p .cache
+          ln -s ${pkgs.callPackage ./deps.nix {}} .cache/p
+          zig build install --cache-dir $(pwd)/.zig-cache --global-cache-dir $(pwd)/.cache -Doptimize=ReleaseSafe --prefix $out
+        '';
+        checkPhase = ''
+          zig build test --cache-dir $(pwd)/.zig-cache --global-cache-dir $(pwd)/.cache
+        '';
       };
     });
 }
