47f438a4f509e2f80662a566eefc72ae0b555798
Asherah Connor <ashe@kivikakk.ee>
2024-09-22 22:09:34 +1000

    adc: use proto, say hi.

--- adc/src/main.zig
+++ adc/src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const serial = @import("serial");
 
+const proto = @import("avacore").proto;
 const Args = @import("./Args.zig");
 
 pub fn main() !void {
@@ -11,7 +12,6 @@ pub fn main() !void {
     var args = try Args.parse(allocator);
     defer args.deinit();
 
-    std.debug.print("opening port ...\n", .{});
     const port_path = "/dev/cu.usbserial-ibU1IGlC1";
     var port = std.fs.cwd().openFile(port_path, .{ .mode = .read_write }) catch |err| switch (err) {
         error.FileNotFound => {
@@ -21,22 +21,15 @@ pub fn main() !void {
     };
     defer port.close();
 
-    std.debug.print("configuring port\n", .{});
     try serial.configureSerialPort(port, .{
         .baud_rate = 1_500_000,
     });
 
-    std.debug.print("writing 01\n", .{});
-    try port.writer().writeAll("\x01");
+    try proto.Request.write(.HELLO, port.writer());
 
-    std.debug.print("reading\n", .{});
-    while (true) {
-        const c = try port.reader().readByte();
-        if (std.ascii.isPrint(c))
-            std.debug.print("{c}", .{c})
-        else
-            std.debug.print("<{x:0>2}>", .{c});
-    }
+    const ev = try proto.Event.read(allocator, port.reader());
+    defer ev.deinit(allocator);
 
-    return 0;
+    std.debug.assert(ev == .VERSION);
+    std.debug.print("connected to {s}\n", .{ev.VERSION});
 }
